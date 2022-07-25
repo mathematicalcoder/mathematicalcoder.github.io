0a123c877edc5a7b7aa78adc8cd07cb084a4dbb5
mathematicalcoder <106592999+mathematicalcoder@users.noreply.github.com>
2022-07-25 17:26:03 +0800

    Create register.php

---
+++ examinatorapp/test/1/register.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>Online Contest Sample | Examinator</title>
+    <link href="https://onlinecontest.mathematicalcoder.repl.co/style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+  <h1>Online Contest Sample</h1>
+  <p class="center">You are about to take the test, if you do not mean to, you may go back to the <a href="https://mathematicalcoder.github.io/mainsite/examinatorapp">homepage</a>. Make sure to read the rules below carefully.</p>
+  <p><b>Rules:</b> <br>
+  <ol>
+  <li> This is a two-question test. There is no time limit.
+  <li> Each problem is worth 1 point.
+  <li> No calculators or resources are allowed.
+  <li> All answers are nonnegative integers.
+  </ol></p>
+    <form action="test.php">
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" name="fname" required><br>
+    <label for="lname">Last name:</label><br>
+    <input type="text" id="lname" name="lname" required> <br> <br>
+    <input type="submit" value="Continue >>"> <br>
+    <p style="font-size:14px;">By entering the test, you agree to answer all problems independently and take the contest honestly.</p>
+    </form>
+  </body>
+</html>
