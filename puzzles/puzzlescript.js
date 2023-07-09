cf6e8f639bb5293b9a2aa7e0a2e8b07c8b5e52b0
Kaiser Chan <106592999+mathematicalcoder@users.noreply.github.com>
2023-07-09 14:01:36 +0800

    Update puzzlescript.js

--- puzzles/puzzlescript.js
+++ puzzles/puzzlescript.js
@@ -1,9 +1,9 @@
 var puzzlelist = [['images/LITS_6_6_1.png', 'nosolpuzzle.png'], ['images/NL_6_6_1.png', 'nosolpuzzle.png']];
 
 function ShowPuzzle(index) {
-  document.getElementById("puzzlepreview").innerHTML = '<img src='.concat(puzzlelist[index][0].concat('></img>'));
+  document.getElementById("puzzlepreview").innerHTML = '<img src='.concat(puzzlelist[index][0].concat(' height="100"></img>'));
 }
 
 function ShowSoln(index) {
-  document.getElementById("solnpreview").innerHTML = '<img src='.concat(puzzlelist[index][1].concat('></img>'));
+  document.getElementById("solnpreview").innerHTML = '<img src='.concat(puzzlelist[index][1].concat(' height="100"></img>'));
 }
