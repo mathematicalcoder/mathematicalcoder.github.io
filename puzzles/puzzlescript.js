b3182bba2a73bc8f34d5891e3a2a1fb1c789130e
Kaiser Chan <106592999+mathematicalcoder@users.noreply.github.com>
2024-02-25 13:50:41 +0800

    Add solutions to puzzles LITS_6_6_1 and NL_6_6_1

--- puzzles/puzzlescript.js
+++ puzzles/puzzlescript.js
@@ -1,4 +1,4 @@
-var puzzlelist = [['images/LITS_6_6_1.png', 'images/nosolpuzzle.png'], ['images/NL_6_6_1.png', 'images/nosolpuzzle.png']];
+var puzzlelist = [['images/LITS_6_6_1.png', 'images/LITS_6_6_1_Sol.png'], ['images/NL_6_6_1.png', 'images/NL_6_6_1_Sol.png']];
 
 function ShowPuzzle(index) {
   document.getElementById("puzzlepreview").innerHTML = '<img src='.concat(puzzlelist[index][0].concat(' height="200" width="200"></img>'));
